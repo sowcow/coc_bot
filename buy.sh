8237375e41dba1932ba1fd248168b2e28b850489
Alexander K <xpyro@ya.ru>
2015-03-24 20:15:55 +0500

    accepts params

--- buy.sh
+++ buy.sh
@@ -1,4 +1,32 @@
 #!/bin/bash --login
-rvm use jruby
-export JRUBY_OPTS=--2.0
-rake buy
+
+#           ↑ login stuff is for rvm
+
+function main {
+  user_param=$1
+
+  use_jruby
+  run_rake_task $user_param
+}
+
+
+function use_jruby {
+  rvm use jruby
+  export JRUBY_OPTS=--2.0
+}
+
+
+function run_rake_task {
+  param=$1
+
+  if [ $param ]; then
+    rake buy[$param]
+  else
+    rake buy
+  fi
+  # not sure if that is necessary though...
+}
+
+
+user_param=$1
+main $user_param
